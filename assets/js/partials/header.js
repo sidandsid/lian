df2084fcc6726a686b5d7b0c09e71480816acc52
Maria M <callmemisha33@gmail.com>
2018-08-06 17:58:20 +0300

    edit: header search fixed

--- assets/js/partials/header.js
+++ assets/js/partials/header.js
@@ -1,25 +1,28 @@
 //Header burger menu
-var headerBurger = $('.js-header-menu__burger'),
-    headerBurgerLine = $('.js-header-menu__burger-line'),
-    headerMenuItems = $('.js-header-menu__items');
+var $headerBurger = $('.js-header-nav__burger'),
+    $headerBurgerLine = $('.js-header-nav__burger-line'),
+    $headerNavItems = $('.js-header-nav__items');
 
-$(headerBurger).on('click', function(){
-    var headerBurgerActive = $(this);
+$headerBurger.on('click', function(){
+    var $headerBurgerActive = $(this);
 
-    $(headerBurgerLine).toggleClass('clicked');
-    $(headerMenuItems).toggleClass('active');
+    //show on burger button click
+    $headerBurgerLine.toggleClass('clicked');
+    $headerNavItems.toggleClass('active');
 
+    //close when clicked elsewhere
     $(document).on('click', function(i){
-        if ($(i.target).is($(headerBurgerActive)) === false){
-            $(headerBurgerLine).removeClass('clicked');
-            $(headerMenuItems).removeClass('active');
+        if ($(i.target).is($headerBurgerActive) === false){
+            $headerBurgerLine.removeClass('clicked');
+            $headerNavItems.removeClass('active');
         }
     });
 
+    //close on Esc button
     $(document).keydown(function(e){
         if(e.keyCode == 27){
-            $(headerBurgerLine).removeClass('clicked');
-            $(headerMenuItems).removeClass('active');
+            $headerBurgerLine.removeClass('clicked');
+            $headerNavItems.removeClass('active');
         }
     });
 });
@@ -30,8 +33,10 @@ var headerSearchIcon = ('.js-header-search__icon'),
     headerSearchClose = ('.js-header-search__btn-close');
 
 $(headerSearchIcon).on('click', function(){
+    //show form on show icon click
     $(headerSearchForm).addClass('active');
 
+    //close on Esc button
     $(document).keydown(function(e){
         if(e.keyCode == 27){
             $(headerSearchForm).removeClass('active');
@@ -39,6 +44,7 @@ $(headerSearchIcon).on('click', function(){
     });
 });
 
+//close on close icon click
 $(headerSearchClose).on('click', function(){
     $(headerSearchForm).removeClass('active');
 });
--- header.php
+++ header.php
@@ -11,34 +11,30 @@
     <?php get_template_part( 'assets/svg/sprite.svg' ); ?>
 
     <header class="header">
-        <!-- Header logo widget -->
         <div class="header-logo">
             <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Header logo") ) : ?>
             <?php endif;?>
         </div>
 
-        <!-- Header navigation -->
         <div class="header-nav">
-            <div class="header-menu">
-                <div class="header-menu__burger js-header-menu__burger">
-                    <span class="header-menu__burger-line js-header-menu__burger-line"></span>
-                </div>
-                <?php
-                    wp_nav_menu( array( 'theme_location' => 'header-navigation', 'container_class' => 'header-menu__items js-header-menu__items' ) );
-                ?>
+            <div class="header-nav__burger js-header-nav__burger">
+                <span class="header-nav__burger-line js-header-nav__burger-line"></span>
             </div>
+            <?php
+                wp_nav_menu( array( 'theme_location' => 'header-navigation', 'container_class' => 'header-nav__items js-header-nav__items' ) );
+            ?>
+        </div>
 
-            <div class="header-search">
-                <div class="header-search__icon js-header-search__icon">
-                    <?php get_template_part('assets/svg/inline', 'search.svg'); ?>
-                </div>
-                <form class="header-search__form js-header-search__form" action="<?php echo home_url( '/' ) ?>" method="get">
-                    <input class="header-search__input" type="text" value="<?php echo get_search_query() ?>" placeholder="Search" name="search">
-                    <button class="header-search__btn search" type="submit">
-                        <?php get_template_part('assets/svg/inline', 'search.svg'); ?>
-                    </button>
-                    <button class="header-search__btn close js-header-search__btn-close" type="button"></button>
-                </form>
+        <div class="header-search">
+            <div class="header-search__icon js-header-search__icon">
+                <?php get_template_part('assets/svg/inline', 'search.svg'); ?>
             </div>
+            <form class="header-search__form js-header-search__form" role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+                <input class="header-search__input" type="search" placeholder="Search" value="<?php echo get_search_query(); ?>" name="s" />
+                <button class="header-search__btn search" type="submit">
+                    <?php get_template_part('assets/svg/inline', 'search.svg'); ?>
+                </button>
+                <button class="header-search__btn close js-header-search__btn-close" type="button"></button>
+            </form>
         </div>
     </header>
